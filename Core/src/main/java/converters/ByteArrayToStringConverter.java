4934216701a56aef43dd1582e35786ff540ed4ae
unknown <wilquintanilha@gmail.com>
2020-07-26 18:42:48 -0300

    entidades do hibernate organizadas

---
+++ Core/src/main/java/converters/ByteArrayToStringConverter.java
@@ -0,0 +1,30 @@
+package converters;
+
+import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Converter
+public class ByteArrayToHexStringConverter implements AttributeConverter<byte[] ,String> {
+
+
+
+    @Override
+    public String convertToDatabaseColumn(byte[] bytes) {
+        return Hex.encodeHexString(bytes);
+    }
+
+    @Override
+    public byte[] convertToEntityAttribute(String dbData) {
+        if (StringUtils.isBlank(dbData))
+            return new byte[0];
+
+        return dbData.getBytes();
+    }
+}
